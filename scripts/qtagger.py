749b413ea7dd3655cbe684a4fa6be853aa1b82e5
Matthew Hartley <mhartley@cantab.net>
2019-11-25 09:16:39 +0000

    Working initial version

--- scripts/qtagger.py
+++ scripts/qtagger.py
@@ -31,9 +31,21 @@ class TaggableImageSet(object):
 
     def __init__(self, uri):
         self.dataset = dtoolcore.DataSet.from_uri(uri)
-        self.tags = {n: 'Untagged' for n in list(range(len(self.dataset.identifiers)))}
+        self.tags = {n: 0 for n in list(range(len(self.dataset.identifiers)))}
         self.load_times = {}
         self.tag_times = {}
+
+    def save_to_file(self, fpath):
+
+        with open(fpath, "w") as fh:
+            fh.write("ImageNumber,TagID,TagLabel,Time\n")
+            for n in range(len(self)):
+                try:
+                    tag_time = self.tag_times[n] - self.load_times[n]
+                except KeyError:
+                    tag_time = -1
+
+                fh.write(f"{n},{self.tags[n]},{TAGMAP[self.tags[n]]},{tag_time}\n")
 
     def __len__(self):
         return len(self.dataset.identifiers)
@@ -56,12 +68,12 @@ class TaggableImageSet(object):
 
 class QTagger(QMainWindow):
 
-    def __init__(self, ds):
+    def __init__(self, uri):
         super(QTagger, self).__init__()
 
-        self.ds = ds
-        self.idns = list(ds.identifiers)
-        self.tags = {idn: 0 for idn in self.idns}
+        self.tis = TaggableImageSet(uri)
+        # self.idns = list(ds.identifiers)
+        # self.tags = {idn: 0 for idn in self.idns}
         self.image_index = 0
 
         self.imageLabel = QLabel()
@@ -78,14 +90,15 @@ class QTagger(QMainWindow):
         self.resize(600, 800)
 
     def set_image(self, idx):
-        idn = self.idns[idx]
-        image_fpath = self.ds.item_content_abspath(idn)
+        # idn = self.idns[idx]
+        # image_fpath = self.ds.item_content_abspath(idn)
+        image_fpath = self.tis[idx]
         image = QImage(image_fpath)
         self.imageLabel.setPixmap(QPixmap.fromImage(image))
 
     def next_image(self):
 
-        if self.image_index+1 < len(self.idns):
+        if self.image_index+1 < len(self.tis):
             self.image_index += 1
             self.set_image(self.image_index)
 
@@ -101,10 +114,10 @@ class QTagger(QMainWindow):
 
     def update_statusbar(self):
 
-        pos_label = f"Image [{self.image_index}/{len(self.idns)}]"
-        current_tag = self.tags[self.idns[self.image_index]]
-        tag_label = TAGMAP[current_tag]
-        
+        pos_label = f"Image [{self.image_index}/{len(self.tis)}]"
+        # current_tag = self.tags[self.idns[self.image_index]]
+        # tag_label = TAGMAP[current_tag]
+        tag_label = f"{TAGMAP[self.tis.tags[self.image_index]]}"
         message = f"{pos_label} - ({tag_label})"
         
         self.statusbar.showMessage(message)
@@ -116,9 +129,16 @@ class QTagger(QMainWindow):
         if event.key() == Qt.Key_Left:
             self.prev_image()
 
+        if event.key() == Qt.Key_Right:
+            self.next_image()
+
+        if event.key() == Qt.Key_S:
+            self.tis.save_to_file("results.csv")
+
         if event.key() in KEYMAP:
             tag_id = KEYMAP[event.key()]
-            self.tags[self.idns[self.image_index]] = tag_id
+            self.tis.tag_item(self.image_index, tag_id)
+            # self.tags[self.idns[self.image_index]] = tag_id
             self.next_image()
 
 
@@ -126,11 +146,11 @@ class QTagger(QMainWindow):
 @click.argument('dataset_uri')
 def main(dataset_uri):
 
-    ds = dtoolcore.DataSet.from_uri(dataset_uri)
+    # ds = dtoolcore.DataSet.from_uri(dataset_uri)
 
     app = QApplication([])
 
-    qtagger = QTagger(ds)
+    qtagger = QTagger(dataset_uri)
     qtagger.show()
 
 
